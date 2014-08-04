086b45e26a8e7bab852550fd7358366480373b56
ThomasJ <tom@axisj.com>
2014-08-04 15:55:41 +0900

    add min & grunt

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+﻿module.exports = function(grunt) {
+
+  // Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		cssmin: {
+			add_banner: {
+				options: {
+					banner: '/*! \n<%= pkg.name %> - v<%= pkg.version %> - ' +
+						'<%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+				},
+				files: {
+					'axicon/axicon.min.css':
+						[
+							'axicon/axicon.css'
+						]
+				}
+			}
+		}
+	});
+	//grunt.loadTasks('tasks');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('css', ['cssmin']);
+};
